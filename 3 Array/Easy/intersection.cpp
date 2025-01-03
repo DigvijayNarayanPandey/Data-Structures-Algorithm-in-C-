e850a774698727a170f9dafbca0de5c958fbe794
Digvijay Narayan Pandey <digvijaynarayanpandey7@gmail.com>
2025-01-03 20:32:45 +0530

    modified

--- 3 Array/Easy/intersection.cpp
+++ 3 Array/Easy/intersection.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 using namespace std;
 
-void intersectionOfTwoAeeays(int *input1, int *input2, int size1, int size2)
-
+void intersectionOfTwoArrays(int *arr1, int *arr2, int size1, int size2)
 {
     for (int i = 0; i < size1; i++)
     {
-        int element = input1[i];
+        int element = arr1[i];
         for (int j = 0; j < size2; j++)
         {
-            if (element == input2[j])
+            if (element == arr2[j])
             {
                 cout << element << " ";
-                input2[j] = -26568;
+                arr2[j] = -26568;
                 break;
             }
         }
     }
 }
+void userInput()
+{
+    int n, m;
+    cout << "Enter the size of the array 1: ";
+    cin >> n;
+    cout << "Enter the size of the array 2: ";
+    cin >> m;
+    int *arr1 = new int[n];
+    cout << "Enter " << n << " elements of array 1:";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
 
+    int *arr2 = new int[m];
+    cout << "Enter " << m << " elements of array 2:";
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
+    cout << "The intersection of two sorted arrays are: ";
+    intersectionOfTwoArrays(arr1, arr2, n, m);
+}
 int main()
 {
-
+    userInput();
     return 0;
 }
